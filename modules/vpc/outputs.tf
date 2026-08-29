4ab7dadf790d1896704a6aeede6567ccd78003e1
thanuja <thanujakularathna0606@gmail.com>
2026-08-29 09:03:08 +0530

    add outputs of subnets'

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,13 @@
 output "vpc_id" {
   value = aws_vpc.this.id
 }
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = [for k, v in aws_subnet.this : v.id if !var.subnets[k].public]
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [for k, v in aws_subnet.this : v.id if var.subnets[k].public]
+}
